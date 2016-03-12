8a5daa9c3ca6d74b39e9544a0246942215138474
CactusTribe <lefrancjoaquim@gmail.com>
2016-03-12 17:36:20 +0100

    Ajout de la selection de la cle railway

--- src/graphic/graphic.c
+++ src/graphic/graphic.c
@@ -7,7 +7,7 @@ void drawWay(SDL_Renderer *ren, OSM_Way *way){
 	RGBA_COLOR rgb_IN;
 	RGBA_COLOR rgb_OUT;
 
-	// Catalogue couleurs
+	// HIGHWAY COLORS
 	RGBA_COLOR primary_IN = {254,216,155,255};
 	RGBA_COLOR primary_OUT = {176,124,0,255};
 	RGBA_COLOR secondary_IN = {243,254,181,255};
@@ -25,50 +25,88 @@ void drawWay(SDL_Renderer *ren, OSM_Way *way){
 	RGBA_COLOR pedestrian_IN = {223,220,234,255};
 	RGBA_COLOR pedestrian_OUT = {168,166,167,255};
 
-	if(strcmp(way->tagList[0].v, "primary") == 0 || strcmp(way->tagList[0].v, "secondary") == 0 || strcmp(way->tagList[0].v, "tertiary") == 0
-		|| strcmp(way->tagList[0].v, "unclassified") == 0 || strcmp(way->tagList[0].v, "residential") == 0 || strcmp(way->tagList[0].v, "service") == 0
-		|| strcmp(way->tagList[0].v, "living_street") == 0 || strcmp(way->tagList[0].v, "pedestrian") == 0){
+	// RAILWAY COLORS
+	RGBA_COLOR tram_IN = {69,69,69,255};
+	RGBA_COLOR tram_OUT = {69,69,69,255};
 
-		
-		if(strcmp(way->tagList[0].v, "primary") == 0){
-			weigth = 25;
-			rgb_IN = primary_IN;
-			rgb_OUT = primary_OUT;
+	char *key = way->tagList[0].k;
+	char *value = way->tagList[0].v;
+
+	if(strcmp(key, "highway") == 0){
+		if(strcmp(value, "primary") == 0 || strcmp(value, "secondary") == 0 || strcmp(value, "tertiary") == 0
+			|| strcmp(value, "unclassified") == 0 || strcmp(value, "residential") == 0 || strcmp(value, "service") == 0
+			|| strcmp(value, "living_street") == 0 || strcmp(value, "pedestrian") == 0){
+
+			
+			if(strcmp(value, "primary") == 0){
+				weigth = 25;
+				rgb_IN = primary_IN;
+				rgb_OUT = primary_OUT;
+			}
+			else if(strcmp(value, "secondary") == 0){
+				weigth = 25;
+				rgb_IN = secondary_IN;
+				rgb_OUT = secondary_OUT;
+			}
+			else if(strcmp(value, "tertiary") == 0){
+				weigth = 25;
+				rgb_IN = tertiary_IN;
+				rgb_OUT = tertiary_OUT;
+			}
+			else if(strcmp(value, "unclassified") == 0){
+				weigth = 15;
+				rgb_IN = unclassified_IN;
+				rgb_OUT = unclassified_OUT;
+			}
+			else if(strcmp(value, "residential") == 0){
+				weigth = 15;
+				rgb_IN = residential_IN;
+				rgb_OUT = residential_OUT;
+			}
+			else if(strcmp(value, "service") == 0){
+				weigth = 8;
+				rgb_IN = service_IN;
+				rgb_OUT = service_OUT;
+			}
+			else if(strcmp(value, "living_street") == 0){
+				weigth = 15;
+				rgb_IN = living_street_IN;
+				rgb_OUT = living_street_OUT;
+			}
+			else if(strcmp(value, "pedestrian") == 0){
+				weigth = 15;
+				rgb_IN = pedestrian_IN;
+				rgb_OUT = pedestrian_OUT;
+			}
+
+			// Draw shape
+		  for(int i=0; i < (way->nb_nodes)-1 ; i++){
+		  	if(i > 0 && i < (way->nb_nodes)-1){
+			  	filledCircleRGBA(ren, way->nodeList[i].lon, way->nodeList[i].lat, (weigth/2),
+			  		rgb_OUT.r, rgb_OUT.g, rgb_OUT.b, rgb_OUT.a);
+			  }
+
+		  	thickLineRGBA(ren, way->nodeList[i].lon, way->nodeList[i].lat, 
+		 			way->nodeList[i+1].lon, way->nodeList[i+1].lat, weigth, rgb_OUT.r, rgb_OUT.g, rgb_OUT.b, rgb_OUT.a);
+		  }
+
+		  // Draw inner shape
+		  for(int i=0; i < (way->nb_nodes)-1 ; i++){
+		  	if(i > 0 && i < (way->nb_nodes)-1){
+			  	filledCircleRGBA(ren, way->nodeList[i].lon, way->nodeList[i].lat, ((weigth-3)/2),
+			  		rgb_IN.r, rgb_IN.g, rgb_IN.b, rgb_IN.a);
+			  }
+
+		  	thickLineRGBA(ren, way->nodeList[i].lon, way->nodeList[i].lat, 
+		 			way->nodeList[i+1].lon, way->nodeList[i+1].lat, weigth-3, rgb_IN.r, rgb_IN.g, rgb_IN.b, rgb_IN.a);
+		  }
 		}
-		else if(strcmp(way->tagList[0].v, "secondary") == 0){
-			weigth = 25;
-			rgb_IN = secondary_IN;
-			rgb_OUT = secondary_OUT;
-		}
-		else if(strcmp(way->tagList[0].v, "tertiary") == 0){
-			weigth = 25;
-			rgb_IN = tertiary_IN;
-			rgb_OUT = tertiary_OUT;
-		}
-		else if(strcmp(way->tagList[0].v, "unclassified") == 0){
-			weigth = 15;
-			rgb_IN = unclassified_IN;
-			rgb_OUT = unclassified_OUT;
-		}
-		else if(strcmp(way->tagList[0].v, "residential") == 0){
-			weigth = 15;
-			rgb_IN = residential_IN;
-			rgb_OUT = residential_OUT;
-		}
-		else if(strcmp(way->tagList[0].v, "service") == 0){
-			weigth = 8;
-			rgb_IN = service_IN;
-			rgb_OUT = service_OUT;
-		}
-		else if(strcmp(way->tagList[0].v, "living_street") == 0){
-			weigth = 15;
-			rgb_IN = living_street_IN;
-			rgb_OUT = living_street_OUT;
-		}
-		else if(strcmp(way->tagList[0].v, "pedestrian") == 0){
-			weigth = 15;
-			rgb_IN = pedestrian_IN;
-			rgb_OUT = pedestrian_OUT;
+	}
+	else if(strcmp(key, "railway") == 0){
+		if(strcmp(value, "tram") == 0){
+			weigth = 9;
+			rgb_IN = tram_IN;
+			rgb_OUT = tram_OUT;
 		}
 
 		// Draw shape
@@ -118,6 +156,7 @@ void OSM_Rendering(SDL_Renderer *ren){
   //OSM_Tag t1 = {"highway", "service"};
   //OSM_Tag t1 = {"highway", "living_street"};
   //OSM_Tag t1 = {"highway", "pedestrian"};
+  //OSM_Tag t1 = {"railway", "tram"};
 
   OSM_Tag *tags_liste = malloc(1 * sizeof(OSM_Tag));
   tags_liste[0] = t1;
